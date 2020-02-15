c1ae5e4e626c538dabed8b60287297304ed120ed
L.Carquet <laetitiacarquet@gmail.com>
2020-02-15 11:30:24 +0100

    still organizig

---
+++ theBasics.rb
@@ -0,0 +1,39 @@
+#6. square numbers
+puts 4.56 * 4.56
+puts 4.56 ** 2
+
+#5. factorial of 5, 6, 7, 8
+# puts 5 * 4 * 3 * 2 * 1
+# puts 6 * 5 * 4 * 3 * 2 * 1
+# puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+# puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+#4. movies in array
+# years = [1222, 1977, 2001]
+# years.each {|year| puts year}  
+
+#3. movies in hash list
+# movieHash = {"le bon, la brute et le truan": 1222, "star wars": 1977, "MOulin Rouge": 2001}
+# movieHash.each do |movie,year| puts year end
+
+#2. 1) thousands place 2) hundreds place 3) tens place 4) ones place
+# number = 3456
+# thousands = number / 1000
+# hundreds = number % 1000 /100
+# tens = number % 100 /10
+# ones = number % 10
+# puts "thousands : #{thousands}"
+# puts "hundreds: #{hundreds}"
+# puts "tens: #{tens}"
+# puts "ones: #{ones}"
+
+#1. Add two strings together that, when concatenated, 
+#return your first and last name as your full name in one string
+# firstName = "Madeleine "
+# lastname = "Bergerac"
+# puts firstName + lastname
+
+
+
+
+
